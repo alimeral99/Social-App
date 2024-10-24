b38c7d1eeb808f4323026886ea907f5e200f71d8
Ali Meral <meralmuhammet544@gmail.com>
2024-10-24 20:59:40 +0300

    Fix erros

---
+++ client/src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div>
+      <h1>test</h1>
+      <h1>test</h1>
+
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
