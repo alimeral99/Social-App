8c8b2398ac71365b6d56fc89f7e8a0513120c2d7
Ali Meral <meralmuhammet544@gmail.com>
2024-10-25 18:43:37 +0300

    Fix haeder component in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <Header />
     </div>
   );
 }
--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,10 +1,39 @@
 import React from "react";
-import "./Header.csss";
+import "./Header.css";
+
+import Avatar from "react-avatar";
+import { AiOutlineHome } from "react-icons/ai";
+import { RiChatFollowUpLine } from "react-icons/ri";
+import { FaRegEdit } from "react-icons/fa";
+import { MdOutlineSearch } from "react-icons/md";
 
 function Header() {
   return (
-    <div>
-      <h1>header</h1>
+    <div className="navbar">
+      <h1 className="navbar-logo">Social app</h1>
+
+      <div className="navbar-links">
+        <div className="icon-container">
+          <AiOutlineHome className="navbar-icon" />
+        </div>
+        <div className="icon-container">
+          <RiChatFollowUpLine className="navbar-icon" />
+        </div>
+        <div className="icon-container">
+          <FaRegEdit className="navbar-icon" />
+        </div>
+      </div>
+
+      <div className="navbar-searchbox">
+        <MdOutlineSearch className="input-icon" />
+        <input type="text" />
+      </div>
+
+      <div className="navbar-profile">
+        <Avatar name="Foo Bar" size="30" round="150px" />
+
+        <button className="profile-button">Add Question</button>
+      </div>
     </div>
   );
 }
