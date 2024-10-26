bf8defcedc23621df469305a1c6b64e5ee61757b
Ali Meral <meralmuhammet544@gmail.com>
2024-10-26 18:48:41 +0300

    Create sidebar component in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import Feed from "./Components/Feed/Feed";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import Widget from "./Components/Widget/Widget";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
+
+      <div className="app-body">
+        <Sidebar />
+
+        <Feed />
+      </div>
+
+      {/* <Widget /> */}
     </div>
   );
 }
---
+++ client/src/Components/Feed/Feed.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default Feed;
