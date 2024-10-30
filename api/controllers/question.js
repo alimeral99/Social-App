d689078d5385bc1ffc40723cbc122a4223719a09
Ali Meral <meralmuhammet544@gmail.com>
2024-10-30 21:17:18 +0300

    Connect the server to database

---
+++ api/controllers/question.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  questionText: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+module.exports = Question;
--- api/index.js
+++ api/index.js
@@ -7,6 +7,15 @@ app.get("/api", (req, res) => {
   res.send("API  working");
 });
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT} `);
 });
