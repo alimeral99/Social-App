775276ac9318be47c5c322d068c99f6fbd9f43bb
Ali Meral <meralmuhammet544@gmail.com>
2025-01-13 14:49:16 +0300

    Update QuestionApi.js

--- client/src/Features/Question/QuestionSlice/QuestionApi.js
+++ client/src/Features/Question/QuestionSlice/QuestionApi.js
@@ -46,7 +46,7 @@ export const likeQuestion = async (questionId, token) => {
 };
 
 export const getQuestionsByCategory = async (category) => {
-  const response = await axios.get("sa-backend-eta.vercel.app/question/search", {
+  const response = await axios.get("socialapp-backend-lemon.vercel.app/question/search", {
     params: { category },
   });
 
