ab9f3cb2f4c31cafed9d336cf437ea9688ce77f3
Ali Meral <meralmuhammet544@gmail.com>
2025-01-13 14:51:04 +0300

    Update TopQuestions.js

--- client/src/Components/Feed/Question/TopQuestions/TopQuestions.js
+++ client/src/Components/Feed/Question/TopQuestions/TopQuestions.js
@@ -13,7 +13,7 @@ function TopQuestions() {
     const fetchQuestions = async () => {
       try {
         const response = await axios.get(
-          `sa-backend-eta.vercel.app/question/most?type=${type}`
+          `socialapp-backend-lemon.vercel.app/question/most?type=${type}`
         );
         console.log(response);
         setQuestions(response.data);
