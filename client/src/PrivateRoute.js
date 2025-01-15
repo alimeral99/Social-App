49fd44064c9b4c732dcb794efd23cefa1ccfb085
Ali Meral <meralmuhammet544@gmail.com>
2025-01-15 13:10:26 +0300

    Update PrivateRoute.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
 
-  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+  return currentUser ? <Outlet /> : <Navigate to="/register" />;
 }
 
 export default PrivateRoute;
